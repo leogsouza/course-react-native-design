8837267b5bc37884642a90e54bfb746607fd4ff8
Leonardo Souza <leogsouza@gmail.com>
2019-11-10 20:29:08 -0300

    Project 4 - Shopping Cart - Add Hero Image

--- 4-shopping-cart/App.js
+++ 4-shopping-cart/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './app/components/Header';
+import HeroImage from './app/components/HeroImage';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <HeroImage />
     </View>
   );
 }
---
+++ 4-shopping-cart/app/components/HeroImage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, Image } from 'react-native'
+
+export default class HeroImage extends Component {
+  render() {
+    return (
+        <Image style={styles.hero} source={require('../img/img2.jpeg')}>
+        
+      </Image>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  hero: {
+    height: 250,
+    width: '100%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+  }
+})
